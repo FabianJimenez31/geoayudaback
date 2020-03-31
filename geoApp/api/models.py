673bd23e6661532b8f23a4f518238a89b4d565b5
sarpn <sarpn47@gmail.com>
2020-03-30 22:14:09 -0500

    Creación preliminar de modelos

--- geoApp/api/models.py
+++ geoApp/api/models.py
@@ -2,20 +2,35 @@ from django.db import models
 
 # Create your models here.
 
+class tipo_entidad(models.Model):
+    nombre = models.CharField(max_length=200)
+
+class tipo_poblacion(models.Model):
+    nombre = models.CharField(max_length=200)
+
+class tipo_iniciativa(models.Model):
+    nombre = models.CharField(max_length=200)
+
 class fase_inicitiva(models.Model):
     nombre = models.CharField(max_length=200)
 
 class nivel_apoyo(models.Model):
     nombre = models.CharField(max_length=200)
 
+class poblacion(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=200)
+
 class iniciativa(models.Model):
-    sector = models.CharField(max_length=200)
     fase = models.ForeignKey(fase_inicitiva, on_delete=models.CASCADE)
     apoyo = models.ForeignKey(nivel_apoyo, on_delete=models.CASCADE)
-    latitud = models.FloatField()
-    longitud = models.FloatField()
+    tipo = ForeignKey(tipo_iniciativa, on_delete = models.CASCADE)
+    poblacion = ForeignKey(poblacion, on_delete = models.CASCADE)
+    entidad = ForeignKey('entidad', on_delete = models.CASCADE)
 
 class entidad(models.Model):
     nombre =  models.CharField(max_length=200)
     dirección =  models.CharField(max_length=200)
-    
+    tipo = models.ForeignKey(tipo_entidad, on_delete=models.CASCADE)
+    latitud = models.FloatField()
+    longitud = models.FloatField()
