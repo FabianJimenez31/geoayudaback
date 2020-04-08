6c862393e858abf2b11fb65faf4c876e69c72caa
Jairo Andres <andresbbx@gmail.com>
2020-04-07 23:38:43 -0500

    agrego swagger a la vista

--- geoApp/api/views.py
+++ geoApp/api/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.parsers import JSONParser
+from drf_yasg.utils import swagger_auto_schema 
+from drf_yasg import openapi
 from .models import iniciativa
 from .serializers import IniciativaSerializer
 # Create your views here.
@@ -10,6 +12,10 @@ from .serializers import IniciativaSerializer
 class EntidadList(APIView):
     parser_classes = (JSONParser,)
 
+    @swagger_auto_schema(
+        responses={200:openapi.Response('Iniciativa',IniciativaSerializer)},
+        tags=['Store']
+    )
     def get(self, request, tipo_iniciativa, format=None):
         iniciativa_object = iniciativa.objects.filter(tipo__nombre=tipo_iniciativa)
         serializer = IniciativaSerializer(many=True)
