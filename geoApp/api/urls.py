e89b7ddecd2c4d459be6c6d998e408dd5fb5ae7e
Nanfe05 <hernan05felipe@gmail.com>
2020-04-14 11:42:32 -0500

    endpoints

--- geoApp/api/urls.py
+++ geoApp/api/urls.py
@@ -3,11 +3,14 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-
-    path('iniciativa/<tipo_iniciativa>/',views.EntidadList.as_view())
-    
-    
-
-    
-    
+    # Return available departments - GET
+    path('departamentos/',views.DepartamentosList.as_view()),
+    # Return available cities - GET
+    path('ciudades/',views.CiudadesList.as_view()),
+    # Return all cities available on department
+    path('ciudades/<departamento_id>',views.CiudadesByDepartment.as_view()),
+    # Return Initiatives by department - POST - Department
+    path('iniciativas/<ciudad_id>/',views.IniciativasByCiudad.as_view()),
+    # Return Initiatives by type
+    path('iniciativa/<tipo_iniciativa>/',views.EntidadList.as_view())  
 ]
--- geoApp/api/views.py
+++ geoApp/api/views.py
@@ -4,8 +4,8 @@ from rest_framework import status
 from rest_framework.parsers import JSONParser
 from drf_yasg.utils import swagger_auto_schema 
 from drf_yasg import openapi
-from .models import iniciativa
-from .serializers import IniciativaSerializer
+from .models import iniciativa,Departamento,Ciudad
+from .serializers import IniciativaSerializer,DepartamentoSerializer,CiudadSerializer
 # Create your views here.
 
 
@@ -18,5 +18,62 @@ class EntidadList(APIView):
     )
     def get(self, request, tipo_iniciativa, format=None):
         iniciativa_object = iniciativa.objects.filter(tipo__nombre=tipo_iniciativa)
-        serializer = IniciativaSerializer(many=True)
+        serializer = IniciativaSerializer(iniciativa_object ,many=True)
+        return Response(serializer.data)
+
+# Get list of available departments
+
+class DepartamentosList(APIView):
+    parser_classes = (JSONParser,)
+
+    @swagger_auto_schema(
+        reponses={200:openapi.Response('Departamentos',DepartamentoSerializer)},
+    )
+
+    def get(self, request, format=None):
+        departamentos = Departamento.objects.all()
+        serializer = DepartamentoSerializer(departamentos ,many=True)
+        return Response(serializer.data)
+
+# Get list of available cities
+
+class CiudadesList(APIView):
+    parser_classes = (JSONParser,)
+
+    @swagger_auto_schema(
+        reponses={200:openapi.Response('Ciudades',CiudadSerializer)},
+    )
+
+    def get(self, request, format=None):
+        ciudades = Ciudad.objects.all()
+        serializer = CiudadSerializer(ciudades ,many=True)
+        return Response(serializer.data)
+
+
+# Get list of available initiatives by department
+
+class CiudadesByDepartment(APIView):
+    parser_classes = (JSONParser,)
+
+    @swagger_auto_schema(
+        reponses={200:openapi.Response('Ciudades',CiudadSerializer)},
+    )
+
+    def get(self, request, departamento_id, format=None):
+        ciudades = Ciudad.objects.filter(departamento=departamento_id)
+        serializer = CiudadSerializer(ciudades ,many=True)
+        return Response(serializer.data)
+
+# Get list of available initiatives by city
+
+class IniciativasByCiudad(APIView):
+    parser_classes = (JSONParser,)
+
+    @swagger_auto_schema(
+        reponses={200:openapi.Response('Iniciativas',IniciativaSerializer)},
+    )
+
+    def get(self, request, ciudad_id, format=None):
+        ciudades = iniciativa.objects.filter(ciudad=ciudad_id)
+        serializer = IniciativaSerializer(ciudades ,many=True)
         return Response(serializer.data)
